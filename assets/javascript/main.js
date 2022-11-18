61f0295b2ff57e72e9aa65300427774c762df8da
Christopher <contato.christopher@gmail.com>
2022-11-18 12:40:11 -0300

    construct fetch function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,9 +2,19 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function (response) {
-    console.log(response)
-})
+fetch(url)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (jsonBody) {
+        console.log(jsonBody)
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
+    .finally(function() {
+        console.log("Finalizado")
+    })
 
 const x = 10 + 10;
 console.log(x);
