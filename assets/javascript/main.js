1a9e39977bd7c628f9693e1cd975797e13e89b01
Christopher <contato.christopher@gmail.com>
2022-11-18 13:11:04 -0300

    add dynamism

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,19 +2,37 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function pokemonToLi (pokemon) {
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>
+    </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
 fetch(url)
-    .then(function (response) {
-        return response.json()
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            
+            pokemonList.innerHTML += pokemonToLi(pokemon);
+        }
     })
-    .then(function (jsonBody) {
-        console.log(jsonBody)
-    })
-    .catch(function (error) {
-        console.log(error)
-    })
-    .finally(function() {
-        console.log("Finalizado")
-    })
+    .catch((error) => console.log(error))
 
 const x = 10 + 10;
 console.log(x);
